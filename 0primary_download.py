da7c39faeabdae3de7f4c6a335309887ee8d97a9
KILLA <KILLA>
2019-05-02 01:20:43 +0530

    Linear Download Time, Estimated 3 seconds per file

--- 0primary_download.py
+++ 0primary_download.py
@@ -1,80 +1,43 @@
 import os
 import wget
-import quandl
 import Helpers
-
 '''
 Om Sai Ram
 
 #0 : Get listings of securities
-#1 : Get past data till date for all securities
-'''
+#1 : Download past data for all symbols into "datasets" folder (4 hours)
+#2 : Process data into sqlite3 database
+#3 : Fill missing data from bhav copy
+#4 : To do
 
+ 
+'''
+def download(code,destfilepath):
+    authkey = "dt4RSh7B_4EvdsMXnuD2"    
+    url = "https://www.quandl.com/api/v3/datasets/BSE/"+code+".csv?api_key="+authkey        
+    wget.download(url,destfilepath)                        
+    
 metadata = Helpers.MetaData()
 codes = metadata.codes
 if not os.path.exists('datasets'):
     os.makedirs('datasets')
-urls = []
-limit = 30
-li = 0
-for (code,name) in codes.items() :    
-    key = "dt4RSh7B_4EvdsMXnuD2"
-    quandl.ApiConfig.api_key = key
-    url = "https://www.quandl.com/api/v3/datasets/BSE/"+code+".csv?api_key="+key    
-    #downloaded_filename = wget.download(url)
-    urls.append(url)
-    li += 1
-    if li > limit :
-        break
+for (code,name) in codes.items() :      
+    try :
+        filepath = os.path.join('datasets',code+'.csv')
+        if not os.path.exists(filepath):
+            download(code,filepath)
+            # import threading
+            # threading.Thread(target=download,args=(code,filepath)).start()        
+            # threading is causing 429 error, too many requests, might get ban            
+        else :
+            print("File Exists:"+code)
+    except :
+        print("Error:"+code)
     
-# Downloading Section Below   
+
+
     
-    
-import sys
-import os
-import urllib
-import threading
-from Queue import Queue
 
-class DownloadThread(threading.Thread):
-    def __init__(self, queue, destfolder):
-        super(DownloadThread, self).__init__()
-        self.queue = queue
-        self.destfolder = destfolder
-        self.daemon = True
-
-    def run(self):
-        while True:
-            url = self.queue.get()
-            try:
-                self.download_url(url)
-            except Exception,e:
-                print "   Error: %s"%e
-            self.queue.task_done()
-
-    def download_url(self, url):
-        # change it to a different way if you require
-        name = url.split('/')[-1]
-        dest = os.path.join(self.destfolder, name)
-        print "[%s] Downloading %s -> %s"%(self.ident, url, dest)
-        urllib.urlretrieve(url, dest)
-
-def download(urls, destfolder, numthreads=4):
-    queue = Queue()
-    for url in urls:
-        queue.put(url)
-
-    for i in range(numthreads):
-        t = DownloadThread(queue, destfolder)
-        t.start()
-
-    queue.join()
-
-if __name__ == "__main__":
-    download(sys.argv[1:], "/tmp")
-    
-    
-    
     
     
     
---
+++ 9email.py
@@ -0,0 +1,9 @@
+import smtplib, ssl
+port = 465
+#password = input("*")
+password = "*"
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    sender = "qq"
+    server.login(sender, password)
+    server.sendmail(sender, "sa", "Hola\nAmigos")    
